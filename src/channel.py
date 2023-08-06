6f5880ce4673d73f1a135c06da7129c08ba10fab
Alex_Volk-Shevalye <aisb@tut.by>
2023-08-06 21:07:52 +0300

    changes for homework #3

--- src/channel.py
+++ src/channel.py
@@ -18,10 +18,33 @@ class Channel:
         self.title = info_channel_json['items'][0]['snippet']['title']
         self.description = info_channel_json['items'][0]['snippet']['description']
         self.url = 'https://www.youtube.com/channel/' + self.__channel_id
-        self.subs_count = info_channel_json['items'][0]['statistics']['subscriberCount']
+        self.subs_count = int(info_channel_json['items'][0]['statistics']['subscriberCount'])
         self.video_count = info_channel_json['items'][0]['statistics']['videoCount']
         self.view_count = info_channel_json['items'][0]['statistics']['viewCount']
 
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return self.subs_count + other.subs_count
+
+    def __sub__(self, other):
+        return self.subs_count - other.subs_count
+
+    def __gt__(self, other):
+        return self.subs_count > other.subs_count
+
+    def __ge__(self, other):
+        return self.subs_count >= other.subs_count
+
+    def __lt__(self, other):
+        return self.subs_count < other.subs_count
+
+    def __le__(self, other):
+        return self.subs_count <= other.subs_count
+
+    def __eq__(self, other):
+        return self.subs_count == other.subs_count
 
     @classmethod
     def get_service(cls):
@@ -39,7 +62,7 @@ class Channel:
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.__channel_id,  part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         Channel.printj(channel)
 
     def to_json(self, file_name):
